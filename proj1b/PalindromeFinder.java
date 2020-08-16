7d24c008bd9c32b601ff83bac9a1811e5d932af0
Bobby Zhang <bobzhang2010@gmail.com>
2020-08-16 00:15:36 -0700

    proj1b complete

--- proj1b/PalindromeFinder.java
+++ proj1b/PalindromeFinder.java
@@ -15,6 +15,24 @@ public class PalindromeFinder {
         }
     }
 
-    /* Uncomment this class once you've written isPalindrome. */
+    /** Uncomment this class once you've written isPalindrome.
+    *
+    * Deque is a interface
+     *
+     * Array Deque and LinkedListDeque implement this interface
+     *
+     *
+     *
+     * In testIsPalindromeCcc() method,
+     *
+     * 1. obo == an instance of "OffByOne" class, "OffByOne" implement cc, use method "equalChars"
+     *
+     * 2. palindrome.isPalindrome(word, obo)
+     * 	3. isPalindrome(wordToDeque(word), obo)
+     * 		4. wordToDeque(word), return a 'Deque'
+     * 			5. isPalindrome('Deque', obo)
+     * 					6. obo.equalChar
+    *
+    */
 
 }
